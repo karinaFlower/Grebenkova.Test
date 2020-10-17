a1e1d293789b30fc44fb79d90cd5d9243958a01b
karinaFlower <72452438+karinaFlower@users.noreply.github.com>
2020-10-17 19:28:31 +0900

    17.10.20

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FunctionInsp.GetInspector("");
+            FunctionInsp.GetСarInspection("");
+            FunctionInsp.SetInspector("");
+            FunctionInsp.GenerateNumber("","","");
+            FunctionInsp.GetWorker("");
+            FunctionInsp.AddWorker("");
+            Console.ReadLine();
+        }
+    }
+}
